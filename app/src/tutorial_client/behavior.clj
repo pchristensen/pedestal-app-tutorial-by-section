e2b45a354f8fca0b61b8cf5b594e56c71d05f612
Peter Christensen <peter@pchristensen.com>
2014-01-08 22:40:22 -0800

    1.3 Increment the Counter

--- app/src/tutorial_client/behavior.clj
+++ app/src/tutorial_client/behavior.clj
@@ -1,10 +1,16 @@
 (ns ^:shared tutorial-client.behavior
     (:require [clojure.string :as string]
-              [io.pedestal.app.messages :as msg]))
+              [io.pedestal.app.messages :as msg]
+              [io.pedestal.app :as app]))
 
 (defn inc-transform [old-value _]
   ((fnil inc 0) old-value))
 
+(defn init-main [_]
+  [[:transform-enable [:main :my-counter] :inc [{msg/topic [:my-counter]}]]])
+
 (def example-app
   {:version 2
-   :transform [[:inc [:my-counter] inc-transform]]})
+   :transform [[:inc [:my-counter] inc-transform]]
+   :emit [{:init init-main}
+          [#{[:*]} (app/default-emitter [:main])]]})
