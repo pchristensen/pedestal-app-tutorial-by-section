cba87d9ce742d5bff906fd5ea926d06cf6f208f4
Peter Christensen <peter@pchristensen.com>
2014-01-18 08:05:35 -0800

    1.11 Rendering#Changing a Value in a Template

--- app/src/tutorial_client/rendering.cljs
+++ app/src/tutorial_client/rendering.cljs
@@ -17,14 +17,20 @@
 (defn render-message [renderer [_ path _ new-value] transmitter]
   (templates/update-t renderer path {:message new-value}))
 
-(defn render-config []
-  [[:node-create  [:main] render-template]
-   [:node-destroy [:main] h/default-destroy]
-   [:transform-enable [:main :my-counter] (h/add-send-on-click "inc-button")]
-   [:transform-disable [:main :my-counter] (h/remove-send-on-click "inc-button")]])
+(defn render-value [renderer [_ path _ new-value] input-queue]
+  (let [key (last path)]
+    (templates/update-t renderer [:main] {key (str new-value)})))
 
 (defn render-template [renderer [_ path] _]
   (let [parent (render/get-parent-id renderer path)
         id (render/new-id! renderer path)
         html (templates/add-template renderer path (:tutorial-client-page templates))]
     (dom/append! (dom/by-id parent) (html {:id id}))))
+
+(defn render-config []
+  [[:node-create  [:main] render-template]
+   [:node-destroy [:main] h/default-destroy]
+   [:transform-enable [:main :my-counter] (h/add-send-on-click "inc-button")]
+   [:transform-disable [:main :my-counter] (h/remove-send-on-click "inc-button")]
+   [:value [:main :*] render-value]
+   [:value [:pedestal :debug :*] render-value]])
