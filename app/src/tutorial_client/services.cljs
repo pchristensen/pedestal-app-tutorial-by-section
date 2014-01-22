ec0a30de7a9f6ad2a7c4648755073e48600db1ff
Peter Christensen <peter@pchristensen.com>
2014-01-21 21:25:14 -0800

    1.13 Connecting to the Service#Implementing the connection to the service

---
+++ app/src/tutorial_client/services.cljs
@@ -0,0 +1,24 @@
+(ns tutorial-client.services
+  (:requre [io.pedestal.app.protocols :as p]
+           [cljs.reader :as reader]))
+
+(defn receive-ss-event [app e]
+  (let [message (reader/read-string (.-data e))]
+    (p/put-message (:input app) message)))
+
+(defrecord Services [app]
+  p/Activity
+  (start [this]
+    (let [source (js/EventSource. "/msgs")]
+      (.addEventListener source
+                         "msg"
+                         (fn [e] (receive-ss-event app e))
+                         false)))
+  (stop [this]))
+
+(defn services-fn [message input-queue]
+  (let [body (pr-str message)]
+    (let [http (js/XMLHttpRequest.)]
+      (.open http "POST" "/msgs" true)
+      (.setRequestHeader http "Content-type" "application/edn")
+      (.send http body))))
