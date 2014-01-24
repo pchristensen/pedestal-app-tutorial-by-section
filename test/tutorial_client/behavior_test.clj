ffedc88ee38fc4c502a1237ca29223c5725a6992
Peter Christensen <peter@pchristensen.com>
2014-01-23 18:27:03 -0800

    Tests (skipped earlier - oops!)

--- test/tutorial_client/behavior_test.clj
+++ test/tutorial_client/behavior_test.clj
@@ -10,32 +10,46 @@
         [io.pedestal.app.query :only [q]]))
 
 ;; Test a transform function
-
-(deftest test-set-value-transform
-  (is (= (set-value-transform {} {msg/type :set-value msg/topic [:greeting] :value "x"})
-         "x")))
+(deftest test-inc-transform
+  (is (= (inc-transform nil {msg/type :inc msg/topic [:my-counter]})
+         1))
+  (is (= (inc-transform 0 {msg/type :inc msg/topic [:my-counter]})
+         1))
+  (is (= (inc-transform 1 {msg/type :inc msg/topic [:my-counter]})
+         2))
+  (is (= (inc-transform 1 nil)
+         2)))
 
 ;; Build an application, send a message to a transform and check the transform
 ;; state
 
+(defn- data-model [app]
+  (-> app :state deref :data-model))
+
 (deftest test-app-state
   (let [app (app/build example-app)]
-    (app/begin app)
-    (is (vector?
-         (test/run-sync! app [{msg/type :set-value msg/topic [:greeting] :value "x"}])))
-    (is (= (-> app :state deref :data-model :greeting) "x"))))
+    (is (test/run-sync! app [{msg/type :inc msg/topic [:my-counter]}]
+                        :begin :default))
+    (is (= (data-model app)
+           {:my-counter 1})))
+  (let [app (app/build example-app)]
+    (is (test/run-sync! app [{msg/type :inc msg/topic [:my-counter]}
+                             {msg/type :inc msg/topic [:my-counter]}
+                             {msg/type :inc msg/topic [:my-counter]}]
+                        :begin :default))
+    (is (= (data-model app)
+           {:my-counter 3}))))
 
 ;; Use io.pedestal.app.query to query the current application model
 
-(deftest test-query-ui
-  (let [app (app/build example-app)
-        app-model (render/consume-app-model app (constantly nil))]
-    (app/begin app)
-    (is (test/run-sync! app [{msg/topic [:greeting] msg/type :set-value :value "x"}]))
-    (is (= (q '[:find ?v
-                :where
-                [?n :t/path [:greeting]]
-                [?n :t/value ?v]]
-              @app-model)
-           [["x"]]))))
-
+;; (deftest test-query-ui
+;;   (let [app (app/build example-app)
+;;         app-model (render/consume-app-model app (constantly nil))]
+;;     (app/begin app)
+;;     (is (test/run-sync! app [{msg/topic [:greeting] msg/type :set-value :value "x"}]))
+;;     (is (= (q '[:find ?v
+;;                 :where
+;;                 [?n :t/path [:greeting]]
+;;                 [?n :t/value ?v]]
+;;               @app-model)
+;;            [["x"]]))))
