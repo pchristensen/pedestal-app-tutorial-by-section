524132cd564a9df04746a51fa353ea3fdf455b7b
Peter Christensen <peter@pchristensen.com>
2014-01-24 16:54:44 -0800

    2.2 Integrating the New Design#Drawing circles

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -66,3 +66,92 @@ var Bars = function(bars) {
     destroy: destroy
   };
 };
+
+var Circles = function(paper, w, h) {
+  var defaultRadius = 20;
+  var padding = 50;
+  var createAnimateTime = 500;
+  var removeAnimateTime = 200;
+  var moveAnimateTime = 1000;
+
+  var reportScoreFn;
+  var removeCounter = 0;
+
+  var removeAll = false;
+
+  var randomPoint = function() {
+    var maxHeight = h - padding;
+    var x = Math.floor(Math.random() * w);
+    if(x < padding)
+      x = padding;
+    var y = Math.floor(Math.random() * h);
+    if(y < padding)
+      y = padding;
+    if(y > maxHeight)
+      y = maxHeight;
+    return {x: x, y: y};
+  };
+
+  var removeCircle = function(c) {
+    if(c && !removeAll) {
+      c.animate({r: 0}, removeAnimateTime, function() {
+        c.remove();
+      });
+    };
+  };
+
+  var moveCircle = function(c) {
+    if (c && !removeAll) {
+      var point = randomPoint();
+      c.animate({"cx": point.x, "cy": point.y}, moveAnimateTime, function() {
+        if (removeCounter > 0) {
+          c.animate({fill: "#000"}, 100);
+          removeCircle(c);
+          removeCounter--;
+        };
+        moveCircle(c);
+      };
+    };
+  };
+
+  var makeCircle = function() {
+    var birth = new Date();
+    var point = randomPoint();
+    var circle = paper.circle(point.x, point.y, 0).attr({
+      fill: "#f00", stroke: "none", opacity: 0.6
+    });
+    circle.animate({r: defaultRadius}, createAnimateTime);
+    moveCircle(circle);
+
+    circle.mouseover(function() {
+      var death = new Date();
+      var t = death - birth;
+      var points = 1;
+      if(t <= 500)
+        points = 3;
+      else if (t <= 1000)
+        points = 2;
+
+      if (reportScoreFn)
+        reportScoreFn(points);
+      removeCircle(circle);
+    });
+  };
+
+  var destroy = function() {
+    removeAll = true;
+  };
+
+  return {
+    addCircle: function() {
+      makeCircle();
+    },
+    removeCircle: function() {
+      removeCounter++;
+    },
+    addScoreReporter: function(f) {
+      reportScoreFn = f;
+    },
+    destroy: destroy
+  };
+};
