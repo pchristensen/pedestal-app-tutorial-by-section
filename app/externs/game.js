b67e7fb1356976ec51e6677519f9e8f805528e19
Peter Christensen <peter@pchristensen.com>
2014-01-26 23:05:46 -0800

    2.3 Rendering the Game#Using an externs file to fix advanced compilation

---
+++ app/externs/game.js
@@ -0,0 +1,10 @@
+var BubbleGame;
+var bubbleGame = {};
+bubbleGame.addHandler = function(callback) {};
+bubbleGame.addPlayer = function(playerName) {};
+bubbleGame.setScore = function(playerName, n) {};
+bubbleGame.setOrder = function(playerName, n) {};
+bubbleGame.setStat = function(statName, n) {};
+bubbleGame.addBubble = function() {};
+bubbleGame.removeBubble = function() {};
+bubbleGame.destroy = function() {};
