2462c38bf0e89cf2f87c113cd2945793cd7197fa
Peter Christensen <peter@pchristensen.com>
2014-01-30 18:11:16 -0800

    2.10 Rendering the Wait Screen#Slicing the wait template

--- app/src/tutorial_client/html_templates.clj
+++ app/src/tutorial_client/html_templates.clj
@@ -4,4 +4,6 @@
 (defmacro tutorial-client-templates
   []
   {:tutorial-client-page (dtfn (tnodes "game.html" "tutorial") #{:id})
-   :login-page (tfn (tnodes "login.html" "login"))})
+   :login-page (tfn (tnodes "login.html" "login"))
+   :wait-page (dtfn (tnodes "wait.html" "wait" [[:#players]]) #{:id})
+   :player (tfn (tnodes "wait.html" "player"))})
