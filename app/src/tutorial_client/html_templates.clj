63fa175c41c668cb5eb9ad7ae2fe69f99e106137
Peter Christensen <peter@pchristensen.com>
2014-01-31 18:11:21 -0800

    2.13 Rendering End of Game#Slice the new Wait Template

--- app/src/tutorial_client/html_templates.clj
+++ app/src/tutorial_client/html_templates.clj
@@ -6,4 +6,5 @@
   {:tutorial-client-page (dtfn (tnodes "game.html" "tutorial") #{:id})
    :login-page (tfn (tnodes "login.html" "login"))
    :wait-page (dtfn (tnodes "wait.html" "wait" [[:#players]]) #{:id})
-   :player (tfn (tnodes "wait.html" "player"))})
+   :player (tfn (tnodes "wait.html" "player"))
+   :high-score (tfn (tnodes "wait.html" "high-score"))})
